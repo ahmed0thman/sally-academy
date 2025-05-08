563eeae18820c7dff71d9ae3368cf3e5254ed200
ahmed0thman <ahmedneo117@gmail.com>
2025-05-08 18:55:16 +0300

    fix menu action

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./style.css";
 import "@/shared/utilities/modal";
 import "@/pages/home/home";
-// import "@/shared/layout/header/header";
+import "@/shared/layout/header/header";
 document.body.classList.add("no-transition");
 window.addEventListener("load", () => {
   document.body.classList.remove("no-transition");
--- src/shared/layout/header/header.js
+++ src/shared/layout/header/header.js
@@ -1,4 +1,12 @@
+const navTogglers = Array.from(document.querySelectorAll(".nav-toggler"));
+
 function toggleMenu() {
   const navMenu = document.getElementById("navMenu");
   navMenu.classList.toggle("open");
+}
+
+if (navTogglers.length > 0) {
+  navTogglers.map((navToggler) =>
+    navToggler.addEventListener("click", toggleMenu)
+  );
 }
